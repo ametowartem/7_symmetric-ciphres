6d1bc4babeb6912a6c62ec456cce5eacde7add92
ametowartem <215750@edu.fa.ru>
2022-12-22 17:41:43 +0300

    finish work

---
+++ hellman.py
@@ -0,0 +1,23 @@
+def client_key_send(a, g, p):
+    A = g ** a % p
+    return A
+
+
+def server_key_send_receive(b, g, p, A):
+    B = g ** b % p
+    K_b = A ** b % p
+    print(K_b)
+    return B
+
+
+def client_receive(B, a, p):
+    K_a = B ** a % p
+    print(K_a)
+
+
+a = 10
+b = 3478
+g = 634
+p = 6589
+
+client_receive(server_key_send_receive(b, g, p, client_key_send(a, g, p)), a, p)
